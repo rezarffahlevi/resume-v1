b1c3019f154bb3c3603b872aaa3a2759421cd54d
Reza Fahlevi <rezarffahlevi@gmail.com>
2019-09-18 10:03:50 +0700

    new template

--- js/main.js
+++ js/main.js
@@ -1,149 +1,237 @@
-;(function () {
-	
-	'use strict';
+(function($) {
+	"use strict";
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
+	$(window).on('load', function() {
+	    $(".preloader").fadeOut("slow", function() {
+	        $(".preloader-left").addClass("slide-left");
+	    });
 
-	
-	var fullHeight = function() {
+	    $('#lionhero').owlCarousel({
+	        animateOut: 'fadeOut',
+	        nav: true,
+	        navText: [
+	            '<i class="ion-ios-arrow-thin-left"></i>',
+	            '<i class="ion-ios-arrow-thin-right"></i>'
+	        ],
+	        items: 1,
+	        navSpeed: 400,
+	        loop: true,
+	        autoplay: true,
+	        autoplayTimeout: 4000,
+	        autoplayHoverPause: true,
+	    });
 
-		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
-			});
-		}
-	};
+	    $('#liontextslider').owlCarousel({
+	        nav: false,
+	        items: 1,
+	        navSpeed: 400,
+	        loop: true,
+	        autoplay: true,
+	        autoplayTimeout: 4000,
+	        autoplayHoverPause: true,
+	    });
 
-	// Parallax
-	var parallax = function() {
-		$(window).stellar();
-	};
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
-					});
-					
-				}, 50);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
-
-
-
-	var goToTop = function() {
-
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
-
-		});
-	
-	};
-
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-
-	};
-
-
-	// Loading page
-	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
-	};
-
-	
-	$(function(){
-		contentWayPoint();
-		goToTop();
-		loaderPage();
-		fullHeight();
-		parallax();
-		// pieChart();
-		skillsWayPoint();
+	    $('#liontestimonial').owlCarousel({
+	        nav: true,
+	        navText: [
+	            '<i class="ion-ios-arrow-thin-left"></i>',
+	            '<i class="ion-ios-arrow-thin-right"></i>'
+	        ],
+	        items: 1,
+	        navSpeed: 400,
+	        loop: true,
+	        autoplay: true,
+	        autoplayTimeout: 4000,
+	        autoplayHoverPause: true,
+	    });
 	});
 
+	$('.portfolio-block, .menu-item').on('click', function() {
 
-}());
+	    //Portfolio masonry
+	    var $container = $('#portfolio-container');
+	    $container.isotope({
+	        masonry: {
+	            columnWidth: '.portfolio-item'
+	        },
+	        itemSelector: '.portfolio-item'
+	    });
+	    $('#filters').on('click', 'li', function() {
+	        $('#filters li').removeClass('active');
+	        $(this).addClass('active');
+	        var filterValue = $(this).attr('data-filter');
+	        $container.isotope({ filter: filterValue });
+	    });
+
+	});
+
+	// Typing Animation (Typed.js)
+	$('#element').typed({
+	    strings: ["UX, UI Designer", "Web App Developer", "Social Animal!"],
+	    typeSpeed: -50,
+	    loop: true,
+	    startDelay: 500,
+	    backDelay: 3000,
+	    contentType: 'html',
+	});
+
+	//Video background
+	// var myPlayer;
+    // $(function () {
+    //   myPlayer = $("#bgndVideo").YTPlayer({useOnMobile:true, mobileFallbackImage:"assets/mask-4.png"});
+
+    // });
+
+	//Portfolio Modal
+	$(document).on('click', '.open-project', function() {
+	    var projectUrl = $(this).attr("href");
+	    $('.inline-menu-container').removeClass('showx');
+	    $('.sidebar-menu').addClass('hidex');
+	    $('.content-blocks.pop').addClass('showx');
+	    $('.content-blocks.pop section').load(projectUrl+' .load-data > *');
+	    return false;
+	});
+
+	//Blog post Modal
+	$('.open-post').on('click', function() {
+	    var postUrl = $(this).attr("href");
+	    $('.inline-menu-container').removeClass('showx');
+	    $('.sidebar-menu').addClass('hidex');
+	    $('.content-blocks.pop').addClass('showx');
+	    $('.content-blocks.pop section').load(postUrl);
+	    return false;
+	});
+
+	//On Click Open Menu Items
+	$('.menu-block, .menu-item').on('click', function() {
+	    $('.name-block').addClass('reverse');
+	    $('.name-block-container').addClass('reverse');
+	    $('.menu-blocks').addClass('hidex');
+	    $('.inline-menu-container').addClass('showx');
+	    $('.inline-menu-container.style2').addClass('dark');
+	});
+	//On Click Open About/Resume Block
+	$('.about-block, .menu-item.about').on('click', function() {
+	    $('.content-blocks').removeClass('showx');
+	    $('.content-blocks.about').addClass('showx');
+	    $('.menu-item').removeClass('active');
+	    $('.menu-item.about').addClass('active');
+	});
+	//On Click Open Portfolio Block
+	$('.portfolio-block, .menu-item.portfolio').on('click', function() {
+	    $('.content-blocks').removeClass('showx');
+	    $('.content-blocks.portfolio').addClass('showx');
+	    $('.menu-item').removeClass('active');
+	    $('.menu-item.portfolio').addClass('active');
+	});
+	//On Click Open Blog Block
+	$('.blog-block, .menu-item.blog').on('click', function() {
+	    $('.content-blocks').removeClass('showx');
+	    $('.content-blocks.blog').addClass('showx');
+	    $('.menu-item').removeClass('active');
+	    $('.menu-item.blog').addClass('active');
+	});
+	//On Click Open Contact Block
+	$('.contact-block, .menu-item.contact').on('click', function() {
+	    $('.content-blocks').removeClass('showx');
+	    $('.content-blocks.contact').addClass('showx');
+	    $('.menu-item').removeClass('active');
+	    $('.menu-item.contact').addClass('active');
+	});
+
+	//On Click Close Blocks
+	$('#close').on('click', function() {
+	    $('.name-block').removeClass('reverse');
+	    $('.name-block-container').removeClass('reverse');
+	    $('.content-blocks').removeClass('showx');
+	    $('.menu-blocks').removeClass('hidex');
+	    $('.inline-menu-container').removeClass('showx');
+	    $('.menu-item').removeClass('active');
+	});
+	//On Click Close Blog Post And Project Details
+	$('#close-pop').on('click', function() {
+	    $('.content-blocks.pop').removeClass('showx');
+	    $('.sidebar-menu').removeClass('hidex');
+	    $('.inline-menu-container').addClass('showx');
+	    $('.content-blocks.pop section').empty();
+	});
+
+	$('.menu-block, .menu-item, #close').on('click', function() {
+	    $('.content-blocks').animate({ scrollTop: 0 }, 800);
+	});	
+
+	//Function for 'Index-Menu2.html'
+	$('#home').on('click', function() {
+	    $('.content-blocks').removeClass('showx');
+	    $('.menu-item').removeClass('active');
+	    $(this).addClass('active');
+	    $('.inline-menu-container.style2').removeClass('dark');
+	});
+
+	// Intialize Map
+	// google.maps.event.addDomListener(window, 'load', init);
+
+	function init() {
+	    // Basic options for a simple Google Map
+	    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+	    var mapOptions = {
+	        // How zoomed in you want the map to start at (always required)
+	        zoom: 11,
+
+	        // The latitude and longitude to center the map (always required)
+	        center: new google.maps.LatLng(40.6700, -73.9400), 
+
+	        scrollwheel: false,
+
+
+	        // How you would like to style the map.
+	        // This is where you would paste any style found on Snazzy Maps.
+	        styles: [{
+	            featureType: 'all',
+	            stylers: [{
+	                saturation: -65
+	            }]
+	        }, {
+	            featureType: 'road.arterial',
+	            elementType: 'geometry',
+	            stylers: [{
+	                hue: '#00ffee'
+	            }, {
+	                saturation: 80
+	            }]
+	        }, {
+	            featureType: 'poi.business',
+	            elementType: 'labels',
+	            stylers: [{
+	                visibility: 'off'
+	            }]
+	        }]
+	    };
+
+	    // Get the HTML DOM element that will contain your map
+	    // We are using a div with id="map" seen below in the <body>
+	    var mapElement = document.getElementById('map');
+
+	    // Create the Google Map using our element and options defined above
+	    var map = new google.maps.Map(mapElement, mapOptions);
+
+	    var image = 'images/map-marker.png';
+	    // Let's also add a marker while we're at it
+	    var marker = new google.maps.Marker({
+	        position: new google.maps.LatLng(40.6700, -73.9400),
+	        map: map,
+	        icon: image,
+	        draggable: true,
+	        animation: google.maps.Animation.DROP
+	    });
+	    marker.addListener('click', toggleBounce);
+
+	    function toggleBounce() {
+	        if (marker.getAnimation() !== null) {
+	            marker.setAnimation(null);
+	        } else {
+	            marker.setAnimation(google.maps.Animation.BOUNCE);
+	        }
+	    }
+	}
+})(jQuery);
